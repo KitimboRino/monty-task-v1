e4cd4f14609f0e55b2d8adc90a9756b64bcba429
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-12-14 14:31:47 +0000

    feat: add mod.c file

--- mod.c
+++ mod.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 
 /**
- * f_mod - computes the rest of the division of the second
+ * f_mod - computes the reminder of the division of the second
  * top element of the stack by the top element of the stack
- * @head: stack head
- * @counter: line_number
+ * @head: pointer to the head of the stack
+ * @counter: line number where the operation is called
  * Return: no return
  */
 void f_mod(stack_t **head, unsigned int counter)
@@ -12,12 +12,15 @@ void f_mod(stack_t **head, unsigned int counter)
 stack_t *h;
 int len = 0, aux;
 
+/* Traverse the satck to counf the number of elements */
 h = *head;
 while (h)
 {
 h = h->next;
 len++;
 }
+
+/* Check if there are at least two elements in the stack */
 if (len < 2)
 {
 fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
@@ -26,7 +29,10 @@ free(bus.content);
 free_stack(*head);
 exit(EXIT_FAILURE);
 }
+
 h = *head;
+
+/* Check if the divisor (top element) is zero */
 if (h->n == 0)
 {
 fprintf(stderr, "L%d: division by zero\n", counter);
@@ -35,8 +41,14 @@ free(bus.content);
 free_stack(*head);
 exit(EXIT_FAILURE);
 }
+
+/* Perfome the modulus operation and update the stack */
 aux = h->next->n % h->n;
 h->next->n = aux;
+
+/* Update the head of the stack to point to the next node */
 *head = h->next;
+
+/* Free the mempry of the removed node */
 free(h);
 }
