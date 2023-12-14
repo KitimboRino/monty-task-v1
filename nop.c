fc224448ae3e02677dfa8a5ad04e62baeb5c398f
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-12-14 16:01:26 +0000

    feat: add mul.c and pall.c files

--- nop.c
+++ nop.c
@@ -1,13 +1,18 @@
 #include "monty.h"
 
 /**
- *f_nop- nothing
- *@head: stack head
- *@counter: line_number
- *Return: no return
+ *f_nop- Does nothing; a NOP for Monty code.
+ *@head: Pointer to the head of the stack.
+ *@counter: Line number where the operation is called.
+ *Return: No return value.
  */
 void f_nop(stack_t **head, unsigned int counter)
 {
-	(void) counter;
-	(void) head;
+/**
+ *The NOP operation does nothing; it is intentionally left empty.
+ *Unused parameters to prevent compiler warnings about unused variables.
+ */
+
+(void) counter;
+(void) head;
 }
