c96ceec62c0347ef5dcd46df5db16bb264f4684f
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-12-14 16:13:51 +0000

    feat: add pint.c file

--- pint.c
+++ pint.c
@@ -1,20 +1,26 @@
 #include "monty.h"
 
 /**
- * f_pint - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * f_pint - Prints the value at the top of the stack
+ * followed by a new line.
+ * @head: Pointer to the head of the stack.
+ * @counter: Line number where the operation is called.
+ * Return: No return value.
  */
+
 void f_pint(stack_t **head, unsigned int counter)
 {
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	printf("%d\n", (*head)->n);
+
+/* Check if the stack is empty*/
+if (*head == NULL)
+{
+fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+fclose(bus.file);
+free(bus.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+
+/* Print the value at the topof the stack*/
+printf("%d\n", (*head)->n);
 }
